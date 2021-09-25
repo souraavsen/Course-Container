53cb8ae6fce04cd2b0ddebde76422608324ff255
souraavsen1 <sourav15-11392@diu.edu.bd>
2021-09-26 02:30:15 +0600

    feat: set up and header added

---
+++ src/Cpmponents/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./header.css"
+
+const Header = () => {
+    return (
+        <div>
+            <h2>Course Container</h2>
+            <p>Here you will find all the course to enhance your Web Development career. Enroll and Start</p>
+            <h3>You Budget: 500</h3>
+        </div>
+    )
+}
+
+export default Header
