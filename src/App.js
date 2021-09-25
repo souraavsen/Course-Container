19e339ae0ee35b40ee0ed6c8a213fd50583c9d28
souraavsen1 <sourav15-11392@diu.edu.bd>
2021-09-26 03:07:26 +0600

    tailwind css set up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Header from './Cpmponents/Header/Header';
+import Courses from './Cpmponents/Courses/Courses'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Header></Header>
+        <div>
+          <Courses/>
+        </div>
       </header>
     </div>
   );
--- src/Cpmponents/Header/Header.js
+++ src/Cpmponents/Header/Header.js
@@ -3,12 +3,15 @@ import "./header.css"
 
 const Header = () => {
     return (
-        <div>
-            <h2>Course Container</h2>
-            <p>Here you will find all the course to enhance your Web Development career. Enroll and Start</p>
-            <h3>You Budget: 500</h3>
-        </div>
-    )
+      <div>
+        <h2 className='font-bold text-4xl py-8'>Course Container</h2>
+        <p className='font-light pb-4'>
+          Here you will find all the course to enhance your Web Development
+          career. Enroll and Start
+        </p>
+        <h3>You Budget: 10000</h3>
+      </div>
+    );
 }
 
 export default Header
